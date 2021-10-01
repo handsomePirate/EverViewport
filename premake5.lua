a5b27bb3f65f5fb4157819c1fc4a2603acade1ae
Vojtěch Kužel <adalbert.kuzel@seznam.cz>
2021-10-01 22:32:34 +0200

    Reconfigured premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -2,42 +2,67 @@ workspace "EverViewport"
 	architecture "x64"
 	configurations { "Debug", "Release" }
 	location "proj"
-
-project "EverViewport"
-	kind "ConsoleApp"
-	staticruntime "off"
-	language "C++"
-	cppdialect "C++17"
-	location "proj"
-	callingconvention "FastCall"
-	targetdir "./build/%{cfg.buildcfg}"
-	objdir "proj/obj/%{cfg.buildcfg}"
-	files { "src/**.hpp", "src/**.cpp", "src/**.glsl" }
-
-	flags {
+	
+	flags
+	{
 		"MultiProcessorCompile"
 	}
 
-	includedirs {
-		"src",
-		"$(VULKAN_SDK)/include"
-	}
-
-	links {
-		"$(VULKAN_SDK)/lib/vulkan-1.lib",
-		"$(VULKAN_SDK)/lib/shaderc_shared.lib"
-	}
+project "EverViewport"
+	kind "StaticLib"
+	staticruntime "off"
+	language "C++"
+	cppdialect "C++17"
+	location "proj/EverViewport"
+	targetdir "./build/%{cfg.buildcfg}"
+	objdir "proj/EverViewport/obj/%{cfg.buildcfg}"
+	files { "src/**.hpp", "src/**.cpp" }
 
 	filter "system:windows"
 		systemversion "latest"
 	filter{}
 	
 	filter "configurations:Debug"
-		defines { "DEBUG" }
+		defines { "EW_DEBUG" }
+		runtime "Debug"
 		symbols "On"
 
 	filter "configurations:Release"
-		defines { "RELEASE" }
+		defines { "EW_RELEASE" }
+		runtime "Release"
+		optimize "On"
+
+	filter {}
+	
+project "Test"
+	kind "ConsoleApp"
+	staticruntime "off"
+	language "C++"
+	cppdialect "C++17"
+	location "proj/Test"
+	targetdir "./Test/build/%{cfg.buildcfg}"
+	objdir "proj/Test/obj/%{cfg.buildcfg}"
+	files { "Test/src/**.hpp", "Test/src/**.cpp" }
+	
+	includedirs {
+		"src",
+		"Test/src"
+	}
+	
+	links {
+		"EverViewport"
+	}
+	
+	filter "system:windows"
+		systemversion "latest"
+	filter{}
+	
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		runtime "Release"
 		optimize "On"
 
 	filter {}
