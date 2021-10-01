5fb956628de1f20f974785924fc9ee1728d4f0fd
Vojtěch Kužel <adalbert.kuzel@seznam.cz>
2021-10-01 20:54:15 +0200

    premake5 setup

---
+++ premake5.lua
@@ -0,0 +1,43 @@
+workspace "ColorFrame"
+	architecture "x64"
+	configurations { "Debug", "Release" }
+	location "proj"
+
+project "ColorFrame"
+	kind "ConsoleApp"
+	staticruntime "off"
+	language "C++"
+	cppdialect "C++17"
+	location "proj"
+	callingconvention "FastCall"
+	targetdir "./build/%{cfg.buildcfg}"
+	objdir "proj/obj/%{cfg.buildcfg}"
+	files { "src/**.hpp", "src/**.cpp", "src/**.glsl" }
+
+	flags {
+		"MultiProcessorCompile"
+	}
+
+	includedirs {
+		"src",
+		"$(VULKAN_SDK)/include"
+	}
+
+	links {
+		"$(VULKAN_SDK)/lib/vulkan-1.lib",
+		"$(VULKAN_SDK)/lib/shaderc_shared.lib"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+	filter{}
+	
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		symbols "On"
+
+	filter "configurations:Release"
+		defines { "RELEASE" }
+		optimize "On"
+
+	filter {}
